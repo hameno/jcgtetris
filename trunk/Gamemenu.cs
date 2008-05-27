dfd89eb82d402c574831f63e8863721880451304
letellier.julien <letellier.julien@2c1ee21c-114e-0410-b4de-05bafd8c07f8>
2008-05-27 10:12:09 +0000

    - einige änderungen   - entfernen der partial "Gamemenu.cs". Inhalt wieder in die Main.cs gepackt   - hinzufügen einer partial "TetrisEssentials.cs". Alle sachen zum eigentlichen tetris-spiel kommen hier rein

--- trunk/Gamemenu.cs
+++ trunk/Gamemenu.cs
@@ -11,19 +11,6 @@ namespace Tetris
 {
     public partial class Main : Form
     {
-        private bool ShowMenu;
-        private bool IsAtMenu
-        {
-            get { return ShowMenu; }
-        }
-        private void DrawMenu(PaintEventArgs e)
-        {
-            e.Graphics.Clear(Color.White);
-            string Title = "JCG Tetris";
-            e.Graphics.DrawString(Title, new Font(FontFamily.GenericSerif, 25f), Brushes.Blue, new PointF((this.ClientSize.Width/2) - Title.Length*(25/3), 20));
-            Rectangle but1 = new Rectangle(this.ClientSize.Width / 3, this.ClientSize.Height / 5, this.ClientSize.Width / 3, this.ClientSize.Height / 10);
-            e.Graphics.FillRectangle(Brushes.LightBlue, but1);
-            e.Graphics.DrawRectangle(Pens.Red, but1);
-        }
+        
     }
 }
